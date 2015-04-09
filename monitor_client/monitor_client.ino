678943b1916fe6a744a81a631fb5ec4f83c6e0ee
lincanbin <lincanbin@hotmail.com>
2015-04-09 21:02:10 +0800

    ADD DSM501A.

--- monitor_client/monitor_client.ino
+++ monitor_client/monitor_client.ino
@@ -2,27 +2,32 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include "Suli.h"
+
+//DHT11
 #include "DHT11.h"
 #define DHT11PIN 2
 dht11 DHT11;
-GPRS gprs(7, 8, BAUDRATE,"cmnet");
-char target_url[61];
+
+//DSM
+int pin = 3;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+//SIM900
+GPRS gprs(7, 8, 9600,"cmnet");
+char target_url[66];
 char buffer[128];
 
 void setup(){
 
 	Serial.begin(9600);
-	Serial.print("Temperature (oC): ");
-	Serial.println((float)DHT11.temperature, 2);
-	Serial.print("Humidity (%): ");
-	Serial.println((float)DHT11.humidity, 2);
-	//snprintf(http_cmd,sizeof(http_cmd),"GET monitor.ourjnu.com/submit.php?device_index=13726247339&longitude=113.540718&latitude=22.256467&temperature=%03d&humidity=%03d&particulate_matter=25 HTTP/1.0\r\n\r\n",DHT11.temperature,DHT11.humidity);
-	//sprintf(http_cmd,"GET /submit.php?device_index=13726247339&longitude=113.540718&latitude=22.256467&temperature=%03d&humidity=%03d&particulate_matter=25 HTTP/1.0\r\n\r\n",DHT11.temperature,DHT11.humidity);
-	//Serial.println(http_cmd);
-	//Use rewrited url to prevent memory overflow
-	snprintf(target_url, sizeof(target_url), "GET /13726247339-113.540718-22.256467-%03d-%03d-25 HTTP/1.0\r\n\r\n",DHT11.temperature,DHT11.humidity);
-	Serial.println(target_url);
-
+        //DSM init
+        pinMode(3,INPUT);
+        starttime = millis();
 
 	gprs.init();
 	// attempt DHCP
@@ -34,6 +39,37 @@ void setup(){
 	// successful DHCP
 	Serial.print("IP Address is ");
 	Serial.println(gprs.getIPAddress());
+}
+
+void loop(){
+
+  	Serial.print("Temperature (oC): ");
+	Serial.println((float)DHT11.temperature, 2);
+	Serial.print("Humidity (%): ");
+	Serial.println((float)DHT11.humidity, 2);
+
+        duration = pulseIn(pin, LOW);
+        lowpulseoccupancy = lowpulseoccupancy+duration;
+      
+        if ((millis()-starttime) > sampletime_ms)
+        {
+                ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+                concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+                //Serial.print(lowpulseoccupancy);
+                // Serial.print(",");
+                Serial.print(ratio);
+                Serial.print(",");
+                Serial.println(concentration);
+                lowpulseoccupancy = 0;
+                starttime = millis();
+        }
+  
+	//snprintf(http_cmd,sizeof(http_cmd),"GET monitor.ourjnu.com/submit.php?device_index=13726247339&longitude=113.540718&latitude=22.256467&temperature=%03d&humidity=%03d&particulate_matter=25 HTTP/1.0\r\n\r\n",DHT11.temperature,DHT11.humidity);
+	//sprintf(http_cmd,"GET /submit.php?device_index=13726247339&longitude=113.540718&latitude=22.256467&temperature=%03d&humidity=%03d&particulate_matter=25 HTTP/1.0\r\n\r\n",DHT11.temperature,DHT11.humidity);
+	//Serial.println(http_cmd);
+	//Use rewrited url to prevent memory overflow
+	snprintf(target_url, sizeof(target_url), "GET /13726247339-113.540718-22.256467-%03d-%03d-%07.2f HTTP/1.0\r\n\r\n",DHT11.temperature, DHT11.humidity, ratio);
+	Serial.println(target_url);
 
 	if(false == gprs.connect(TCP,"monitor.ourjnu.com", 80)) {
 		Serial.println("connect error");
@@ -55,17 +91,14 @@ void setup(){
 		Serial.print(" bytes: ");
 		Serial.println(buffer);
 	}
-	gprs.close();
-	gprs.disconnect();
+	//gprs.close();
+	//gprs.disconnect();
 
-}
-
-void loop(){
 	/*
 	while(!gprs.available()) {
 	gprs.write("AT+CGATT?");
 	delay(2000);
 	}
 	Serial.println(gprs.read());*/
-	delay(100000);
+	delay(30000);
 }
